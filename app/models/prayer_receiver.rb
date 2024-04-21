0b9e256b42c107044c67571a653c466dd081ac82
Artur Sulej <artur@fresha.com>
2024-04-21 23:34:54 +0200

    Validations

--- app/models/prayer_receiver.rb
+++ app/models/prayer_receiver.rb
@@ -1,2 +1,8 @@
 class PrayerReceiver < ApplicationRecord
+  validates :first_name, presence: true
+  validates :end_date, presence: true
+  validates :email,
+    format: { with: /\A(.+)@(.+)\z/, message: "Email invalid" },
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 4, maximum: 254 }
 end
